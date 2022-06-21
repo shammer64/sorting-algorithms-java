be9da542bbfc10ed173c4c2c53d800a3f37ac01b
Scott Hammer <shammer64@gmail.com>
2022-06-21 17:13:35 -0400

    Extracted abstract superclass SorterTest in order to share tests across algorithms

--- src/test/java/com/shammer/QuickSorterTest.java
+++ src/test/java/com/shammer/QuickSorterTest.java
@@ -9,100 +9,12 @@ import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class QuickSorterTest {
+public class QuickSorterTest extends SorterTest {
 
-    private Sorter sorter;
-
+    @Override
     @BeforeEach
     public void initialize() {
         sorter = new QuickSorter();
-    }
-
-    @Test
-    public void shouldQuicklyReturnEmptyArray() {
-        int[] expected = new int[0];
-
-        int[] actual = sorter.sort(new int[0]);
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldQuicklyReturnArrayOfOne() {
-        int[] expected = new int[] {15};
-
-        int[] actual = sorter.sort(new int[] {15});
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortOrderedArrayOfTwo() {
-        int[] expected = new int[] {15, 30};
-
-        int[] actual = sorter.sort(new int[] {15, 30});
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortUnorderedArrayOfTwo() {
-        int[] expected = new int[] {15, 30};
-
-        int[] actual = sorter.sort(new int[] {30, 15});
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortUnorderedArrayOfThree() {
-        int[] expected = new int[] {15, 30, 45};
-
-        int[] actual = sorter.sort(new int[] {45, 15, 30});
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortUnorderedArrayOfTen() {
-        int[] expected = new int[] {9, 17, 18, 23, 32, 39, 46, 50, 61, 73};
-
-        int[] actual = sorter.sort(new int[] {23, 50, 9, 18, 61, 32, 17, 73, 46, 39});
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortUnorderedArrayOfOneHundred() {
-        int[] randomInputArray = IntStream.generate(() -> new Random().nextInt(1000))
-                .limit(100).toArray();
-        int[] expected = Arrays.stream(randomInputArray).sorted().toArray();
-
-        int[] actual = sorter.sort(randomInputArray);
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortUnorderedArrayOfOneThousand() {
-        int[] randomInputArray = IntStream.generate(() -> new Random().nextInt(1000))
-                .limit(1000).toArray();
-        int[] expected = Arrays.stream(randomInputArray).sorted().toArray();
-
-        int[] actual = sorter.sort(randomInputArray);
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSortUnorderedArrayOfOneHundredThousand() {
-        int[] randomInputArray = IntStream.generate(() -> new Random().nextInt(1000))
-                .limit(100000).toArray();
-        int[] expected = Arrays.stream(randomInputArray).sorted().toArray();
-
-        int[] actual = sorter.sort(randomInputArray);
-
-        assertArrayEquals(expected, actual);
     }
 
 }
